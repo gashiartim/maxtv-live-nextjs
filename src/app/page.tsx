fb0f48f753a81efd832ca46bdc26541fd16e4dbd
Artim Gashi <73635267+gashiartim@users.noreply.github.com>
2024-07-01 10:31:41 +0200

    Init Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center flex-col">
+      {/* Lets add a currently underwork page: */}
+      <div className="font-bold text-3xl">MAXTV LIVE</div>
+      <h1 className="text-xl">Website currently under construction</h1>
+    </main>
+  );
+}
